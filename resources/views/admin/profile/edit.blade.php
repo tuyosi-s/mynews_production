199725b00c3b4f74aa3d2810c6f7b088ab019370
tuyosi-s <t.umebosi.suzuki@gmail.com>
2024-01-17 10:47:36 +0000

    ブーストコース、ベーシックタームの課題２０提出

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edgs">
+       <meta name="viewport" content="width=device-width,initial-scale=1">
+      
+       <titie>ブーストコース　ベーシックターム課題20-4</title>
+    </head>
+    <body>
+       <h1>ProfileControllerのeditアクションに必要なディレクトリ・ファイルの作成とHTMLでの記述</h1>
+    </body>
+    
+</html>
