2139f6977a02540f43b5b56b2c9cdf9d7ae924f6
tuyosi-s <t.umebosi.suzuki@gmail.com>
2024-01-23 06:37:12 +0000

    ブーストコース、ベーシックタームNo.21課題提出

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html>
-    
-    <head>
-       <meta charset="utf-8">
-       <meta http-equiv="X-UA-Compatible" content="IE=edgs">
-       <meta name="viewport" content="width=device-width,initial-scale=1">
-      
-       <titie>ブーストコース　ベーシックターム課題20-4</title>
-    </head>
-    <body>
-       <h1>ProfileControllerのaddアクションに必要なディレクトリ・ファイルの作成とHTMLでの記述</h1>
-    </body>
-    
-</html>
+{{--ベーシックターム課題No21-4：課題３で作成した profile.blade.phpファイルを読み込む。--}}
+@extends('layouts.profile')
+
+{{--ベーシックターム課題No.21-4：プロフィールのページであることがわかるように titleとcontentを編集する--}}
+ {{--admin.blade.phpの@yeld('title')に'ニュースの新規作成'を埋め込む--}}
+       @section('title','プロフィールのページ')
+       
+       {{--admin.blade.phpの@yield('content')に以下のタグを埋め込む--}}
+       @section('content')
+          <div class ="container">
+              <div class ="row">
+                  <div class ="col-md-8 mx-auto">
+                     <h2>プロフィール</h2> 
+                  </div>
+              </div>
+          </div>
+       @endsection
