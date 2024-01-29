92731c31718fadde3e5c49a2ef660723c5e68fda
tuyosi-s <t.umebosi.suzuki@gmail.com>
2024-01-29 01:17:55 +0000

    ブーストコース、ベーシックタームphp No.22提出

---
+++ lang/ja/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'login' => 'ログイン',
+    'logaut' => 'ログアウト',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+    'remember' => 'ログイン情報を追記する',
+];
--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,15 @@ Route::get('/', function () {
 use App\Http\Controllers\Admin\NewsController;
 
 Route::controller(NewsController::class)->prefix('admin')->group(function(){
-    Route::get('news/create','add');
+    Route::get('news/create','add')->middleware('auth');
 });
    
 // ブーストコース　ベーシックタームNo19-4課題実施。
 use App\Http\Controllers\Admin\ProfileController;
 
 Route::controller(ProfileController::class)->prefix('admin')->group(function(){ 
-    Route::get('profile/create','add');
-    Route::get('profile/edit','edit');
-
+    Route::get('profile/create','add')->middleware('auth');
+    Route::get('profile/edit','edit')->middleware('auth');
 });
 
 //No19-3課題実施
