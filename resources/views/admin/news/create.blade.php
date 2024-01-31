7c8c574c3337ac998ed8f4b94336b8b9b227c460
tuyosi-s <t.umebosi.suzuki@gmail.com>
2024-01-31 02:11:58 +0000

    ブーストコース　LaravelNo.13課題の提出

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -9,7 +9,37 @@
           <div class ="container">
               <div class ="row">
                   <div class ="col-md-8 mx-auto">
-                     <h2>ニュース新規作成</h2> 
+                     <h2>ニュース新規作成</h2>
+                     <form action="{{route('admin.news.create')}}" method="post" enctype="multipart/form-data">
+                         @if(count($errors) > 0) 
+                            <ul>
+                                @foreach($errors->all() as $e)
+                                  <li>{{ $e }}</li>
+                                @endforeach
+                            </ul>
+                         @endif
+                         <div class="form-group row">
+                             <label classs="col-md-2">タイトル</lavel>
+                             <div class="col-md-10">
+                                 <input type="text" class="lorm-control" name="title" value="{{ old('title')}}">
+                             </div>
+                         </div>
+                         
+                         <div class="form-group row">
+                             <lavel class="col-md-2">本文</lavel>
+                             <div class="col-md-10">
+                                 <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                             </div>
+                         </div>
+                         <div class="form-group row">
+                              <lavel class="col-md-2">画像</lavel>
+                              <div class="col-md-10">
+                                  <input type="file" class="form-control-file" name="image">
+                              </div>
+                              @csrf
+                              <input type="submit" class="btn btn-primary" value="更新">
+                     </form>
+    
                   </div>
               </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,19 @@ Route::get('/', function () {
     
 use App\Http\Controllers\Admin\NewsController;
 
-Route::controller(NewsController::class)->prefix('admin')->group(function(){
-    Route::get('news/create','add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('news/create','add')->name('news.add');
+    Route::post('news/create','create')->name('news.create');
 });
    
 // ブーストコース　ベーシックタームNo19-4課題実施。
 use App\Http\Controllers\Admin\ProfileController;
 
-Route::controller(ProfileController::class)->prefix('admin')->group(function(){ 
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->group(function(){ 
     Route::get('profile/create','add')->middleware('auth');
+    Route::post('profile/create','create')->name('profile.create');
     Route::get('profile/edit','edit')->middleware('auth');
+    Route::post('profile/edit','update')->name('profile.update');
 });
 
 //No19-3課題実施
